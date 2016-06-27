807d9c594d2f0b38880b9e545dc5ade132d43460
Nick.Richards <nicholas.richards.6@us.af.mil>
2016-06-27 12:52:33 -0400

    Adding linus tribute page

---
+++ tribute-page/linus-tribute.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".jumbotron").css("background-color", "beige");
+  $(".well").css("background-color", "burlywood");
+  $(".well").css("border-radius","10px");
+  $("body").css("margin","30px");
+  $("hr").css("color", "burlywood");
+  $("hr").css("width", "80%");
+});
