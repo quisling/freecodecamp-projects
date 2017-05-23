1f6c3cf8aa3d12e9e6655ba93a2188c0ff300dc5
richarnj <nicholas.richards.6@us.af.mil>
2017-05-23 11:16:22 -0400

    Starting new tic-tac-toe project, basic layout in place

---
+++ tic-tac-toe/tic-tac-toe.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var isO = 0,
+    goesFirst = math.round(math.random()),
+
+getRandomSquare = () =>{
+	alert(goesFirst);
+};
+
+$(document).ready(function() {
+	getRandomSquare();
+	//document.getElementById("play-area").innerHTML = goesFirst;
+});
