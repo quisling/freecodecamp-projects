b045898278dcdd58eecc36888f837eab3dad21f3
richarnj <nicholas.richards.6@us.af.mil>
2017-05-23 13:20:30 -0400

    updated look and feel to more easily see for debugging purposes. Added random start icon, fixed random ai play.

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,22 +1,80 @@
 "use strict";
 
-var isO = 0,
-    //goesFirst = math.round(math.random()),
-    playArea = [['1','2','3'],['4','5','6'],['7','8','9']],
+var isPlayerO = Math.floor(Math.random()*2),
+    aiChar,
+    playerChar,
+    playerGoesFirst = 0, //Math.round(Math.random()),
+    playArea = [['','',''],['','',''],['','','']],
+    nextSquare = [0,0],
+    winner = false,
 
-getRandomSquare = () =>{
-	alert(goesFirst);
+initAI = () =>{
+   if (isPlayerO){
+     aiChar = 'X';
+     playerChar = 'O';
+  } else {
+     aiChar = 'O';
+     playerChar = 'X';
+  };
+
+  // Temp Board Setup
+  for (let i=0; i<3; i++){
+    for (let j=0; j<3; j++){
+       playArea[i][j] = playerChar;
+    }
+  } 
+
+  if (!playerGoesFirst){
+      aiTurn();
+   };
 },
+
+checkWin = () =>{
+   winner = true;
+},
+
+setSquare = (xComp, yComp, toSet) =>{
+   playArea[xComp][yComp] = toSet;
+   drawBoard();
+   checkWin();
+},
+
+aiTurn = () =>{
+   setNextSquareRandom();
+   setSquare(nextSquare[0], nextSquare[1], aiChar);
+},
+
+playTurn = (event) =>{
+   
+},
+
+setNextSquareRandom = () =>{
+	let getRand = () =>{
+      return Math.floor(Math.random()*3);
+   };
+
+   for (let i=0; i<2; i++){
+      nextSquare[i] = getRand();
+   };
+},
+
 drawBoard = () =>{
    for (let i=0; i<3; i++){
       for (let j=0; j<3; j++){
          document.getElementById("space" + i.toString() + j.toString()).innerHTML = playArea[i][j];
       }
    }
+},
+
+getInput = () =>{
+   // Do input
+   aiTurn();
 };
 
 $(document).ready(function() {
 	drawBoard();
-   getRandomSquare();
-	//document.getElementById("play-area").innerHTML = goesFirst;
+   initAI();
+   while (!winner){
+      getInput();
+   };
 });
