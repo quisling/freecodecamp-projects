98b3c23b8ebe0c4907dc851e193b2f2b15212f58
richarnj <nicholas.richards.6@us.af.mil>
2017-05-23 15:25:39 -0400

    now detects wins! this is brute force and not-great, but it is confirmed working for now

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -3,7 +3,7 @@
 var isPlayerO = Math.floor(Math.random()*2),
     aiChar,
     playerChar,
-    playerGoesFirst = 0, //Math.round(Math.random()),
+    playerGoesFirst = 0, //Math.floor(Math.random()*2),
     playArea = [['&nbsp;','&nbsp;','&nbsp;'],['&nbsp;','&nbsp;','&nbsp;'],['&nbsp;','&nbsp;','&nbsp;']],
     nextSquare = [0,0],
     winner = false,
@@ -17,26 +17,96 @@ initAI = () =>{
      playerChar = 'X';
   };
 
-  /* Temp Board Setup
-  for (let i=0; i<3; i++){
-    for (let j=0; j<3; j++){
-       playArea[i][j] = playerChar;
-    }
-  } */
-
   if (!playerGoesFirst){
       aiTurn();
    };
 },
 
-checkWin = () =>{
-   winner = true;
+checkWin = (player) =>{
+   let checkVal;
+   if (player){
+      checkVal = playerChar;
+   } else {
+      checkVal = aiChar;
+   }
+
+   if (playArea[0][0] === checkVal){
+      if (playArea[0][1] === checkVal){
+         if (playArea[0][2] === checkVal){
+            // Top Row
+            return true;
+         }
+      }
+      if (playArea[1][0] === checkVal){
+         if (playArea[2][0] === checkVal){
+            // Left Col
+            return true;
+         }
+      }
+      if (playArea[1][1] === checkVal){
+         if (playArea[2][2] === checkVal){
+            //Top L to Bot R Diag
+            return true;
+         }
+      }
+   }
+   if (playArea[0][1] === checkVal){
+      if (playArea[1][1] === checkVal){
+         if (playArea[2][1] === checkVal){
+            // Mid Col
+            return true;
+         }
+      }
+   }
+   if (playArea[0][2] === checkVal){
+      if (playArea[1][2] === checkVal){
+         if (playArea[2][2] === checkVal){
+            // Right Col
+            return true;
+         }
+      }
+      if (playArea[1][1] === checkVal){
+         if (playArea[2][0] === checkVal){
+            // Top R to Bot L Diag
+            return true;
+         }
+      }
+   }
+   if (playArea[1][0] === checkVal){
+      if (playArea[1][1] === checkVal){
+         if (playArea[1][2] === checkVal){
+             // Mid Row
+             return true;
+         }
+      }
+   }
+   if (playArea[2][0] === checkVal){
+      if (playArea[2][1] === checkVal){
+         if(playArea[2][2] === checkVal){
+            // Bot Row
+            return true;
+         }
+      }
+   }
+   return false;
+},
+    
+checkEnd = () =>{
+   if (checkWin(1)){
+      winner = true;
+      alert("Player Wins!");
+   } else if (checkWin(0)){
+      winner = true;
+      alert("AI Wins!");
+   } else{
+      winner = false;
+   }
 },
 
 setSquare = (xComp, yComp, toSet) =>{
    playArea[xComp][yComp] = toSet;
    drawBoard();
-   checkWin();
+   checkEnd();
 },
 
 aiTurn = () =>{
@@ -45,16 +115,23 @@ aiTurn = () =>{
 },
 
 playTurn = (event) =>{
-   
+   alert(event);
 },
 
 setNextSquareRandom = () =>{
-	let getRand = () =>{
-      return Math.floor(Math.random()*3);
+   let full = true,
+       getRand = () =>{
+         return Math.floor(Math.random()*3);
    };
-
-   for (let i=0; i<2; i++){
-      nextSquare[i] = getRand();
+   
+   while (full){
+      for (let i=0; i<2; i++){
+         nextSquare[i] = getRand();
+      };
+      let testVal = playArea[nextSquare[0]][nextSquare[1]];
+      if ((testVal != 'O') && (testVal != 'X')) {
+          full = false;
+      };
    };
 },
 
@@ -72,9 +149,10 @@ getInput = () =>{
 };
 
 $(document).ready(function() {
+   //$(" div[id^=space] ").on("click", playTurn());
 	drawBoard();
    initAI();
-   while (!winner){
+   //while (!winner){
       getInput();
-   };
+   //};
 });
