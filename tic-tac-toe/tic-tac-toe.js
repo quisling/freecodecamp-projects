e152a30deba7f5a7c82fb11223f415bbe3ab4801
richarnj <nicholas.richards.6@us.af.mil>
2017-05-23 11:31:07 -0400

    basic board created in bootstrap, basic draw board stub in js

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -2,12 +2,21 @@
 
 var isO = 0,
     goesFirst = math.round(math.random()),
+    playArea = [['','',''],['','',''],['','','']],
 
 getRandomSquare = () =>{
 	alert(goesFirst);
+},
+drawBoard = () =>{
+   for (let i=0; i<3; i++){
+      for (let j=0; j<3; j++){
+         document.getElementById("space"+i+j).innerHTML = playArea[i,j];
+      }
+   }
 };
 
 $(document).ready(function() {
-	getRandomSquare();
+	drawBoard();
+   getRandomSquare();
 	//document.getElementById("play-area").innerHTML = goesFirst;
 });
